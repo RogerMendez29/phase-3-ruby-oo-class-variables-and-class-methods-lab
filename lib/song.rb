324d2a2b2763291698f08c33806a2a61a0865003
Roger Mendez <roger61087@gmail.com>
2021-12-27 15:54:23 -0500

    commit tree 1d77ec6b79481216ee1c5ad0bb7b146336142645

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists =[]
+    @@genres =[]
+    @@genre_count = {}
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+        @@artist_count={}
+    end
+
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.genre_count
+        @@genre_count = @@genres.tally
+    end
+    def self.artist_count
+        @@artist_count = @@artists.tally
+    end
+
+    
+end
